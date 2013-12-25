8b1a7cc07d31324a356c284f53a28b688d20b373
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2013-12-25 17:28:25 -0600

    More Fixes

--- juicer-web/src/main/scala/net/matthaynes/juicer/web/ApiServlet.scala
+++ juicer-web/src/main/scala/net/matthaynes/juicer/web/ApiServlet.scala
@@ -9,16 +9,7 @@ class ApiServlet extends JsonServlet {
   get("/ping")        { Map("message"  -> "pong") }
 
   get("/article")     { Map("article"  -> service.extract(params("url"))) }
-/*
-  get("/articles")     { 
-  	val response = List()
-  	params("urls").foreach(
-		url => response() = service.extract(url)
-	)
-
-  	Map("articles"  -> response) 
-  }
-*/
-  post("/entities")   { Map("entities" -> service.entities.classify(params("text"))) }
+  
+  //post("/entities")   { Map("entities" -> service.entities.classify(params("text"))) }
 
 }
